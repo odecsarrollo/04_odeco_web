42994a0d9f6a5cf52d419ac0418eff25993bfe83
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2017-10-27 17:57:20 -0500

    Cambio correo from en contactenos

--- web/views.py
+++ web/views.py
@@ -9,8 +9,6 @@ from django.views.generic import TemplateView, View
 from web_empresa.models import Aliado, GaleriaFotoEmpresa
 from web_clientes.models import Cliente
 
-from django.conf import settings
-
 
 @method_decorator(gzip_page, name='dispatch')
 class IndexView(TemplateView):
@@ -65,7 +63,7 @@ class SendContactenosView(View):
         email = EmailMessage(
             subject=asunto,
             body=mensaje,
-            from_email='Contactenos Odecopack SAS<odecopack@odecopack.com>',
+            from_email='Contactenos Odecopack SAS<webmaster@odecopack.co>',
             to=['odecopack@odecopack.com'],
             reply_to=[correo]
         )
@@ -75,7 +73,7 @@ class SendContactenosView(View):
         email2 = EmailMessage(
             subject='Su envío de correo a Odecopack',
             body=mensaje2,
-            from_email='Contactenos Odecopack SAS<odecopack@odecopack.com>',
+            from_email='Contactenos Odecopack SAS<webmaster@odecopack.co>',
             to=[correo]
         )
         email2.send()
