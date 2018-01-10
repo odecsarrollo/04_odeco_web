1d5244d6548b56bf26669c3f9b150cc1ac35ccc4
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2018-01-10 11:37:56 -0500

    Mejora en query para activos en casos de éxito

--- web_casos_exito/views.py
+++ web_casos_exito/views.py
@@ -7,5 +7,14 @@ from .models import CasoExito
 class CasosExitoListView(ListView):
     model = CasoExito
     template_name = 'web/casos_exito/casos_exito_list.html'
-    queryset = CasoExito.objects.select_related('industria', 'cliente').all()
+    queryset = CasoExito.objects.filter(
+        activo=True
+    ).select_related(
+        'industria',
+        'cliente'
+    ).prefetch_related(
+        'mis_testimonios',
+        'mis_imagenes',
+        'mis_videos'
+    ).all()
     context_object_name = 'casos_exito_lista'
